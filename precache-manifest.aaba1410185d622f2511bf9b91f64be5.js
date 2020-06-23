d0fb00187fc421ee865ed5d8e5b627fe6dc65546
Tobiasz Maj <tobiaszmaj1@gmail.com>
2020-06-23 02:11:09 +0200

    Updates

---
+++ precache-manifest.aaba1410185d622f2511bf9b91f64be5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1313678a574f7cc07da3ed8f93e515a1",
+    "url": "/5-day-weather-app/index.html"
+  },
+  {
+    "revision": "fcf3910bcf42c6d97453",
+    "url": "/5-day-weather-app/static/js/2.9fa25959.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/5-day-weather-app/static/js/2.9fa25959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46724ae12aa52933ad3b",
+    "url": "/5-day-weather-app/static/js/main.11449aab.chunk.js"
+  },
+  {
+    "revision": "db3d407cac78ce006b65",
+    "url": "/5-day-weather-app/static/js/runtime-main.fe2b1c9b.js"
+  },
+  {
+    "revision": "ed1e6c52406c0dd51f214366944d0df4",
+    "url": "/5-day-weather-app/static/media/logo.ed1e6c52.svg"
+  }
+]);
